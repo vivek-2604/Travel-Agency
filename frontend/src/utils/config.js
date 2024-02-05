c9f1edacad7b6fc8e2c601ce03bce35183181531
vivek mashru <mashruvivek04@gmail.com>
2024-02-05 12:39:01 +0530

    change in config

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
-// export const  BASE_URL = "https://travel-agency-xi-murex.vercel.app"
-export const  BASE_URL = "http://localhost:8000"
+export const  BASE_URL = "https://travel-agency-xi-murex.vercel.app"
+// export const  BASE_URL = "http://localhost:8000"
