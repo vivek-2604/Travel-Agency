4e2825996e62e7beda1be806d27b1bcce042b84c
vivek mashru <mashruvivek04@gmail.com>
2024-02-23 13:02:35 +0530

    change in cors link

--- backend/index.js
+++ backend/index.js
@@ -50,7 +50,7 @@ app.use(bodyParser.json());
 
 
 const corsOption = {
-  origin: "https://travel-agency-7pt8.vercel.app/home",
+  origin: "https://travel-agency-7pt8.vercel.app",
   optionsSuccessStatus : 200,
   credentials: true,
 };
